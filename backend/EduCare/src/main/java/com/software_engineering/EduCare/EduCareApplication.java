fe947609e79a95ba4deed13c8dde163c3137a506
Lucas Calumbi <lucascalumbi30@gmail.com>
2026-01-05 23:26:40 -0300

    creating project structure and backend java spring project

---
+++ backend/EduCare/src/main/java/com/software_engineering/EduCare/EduCareApplication.java
@@ -0,0 +1,13 @@
+package com.software_engineering.EduCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduCareApplication.class, args);
+	}
+
+}
---
+++ backend/EduCare/src/test/java/com/software_engineering/EduCare/EduCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software_engineering.EduCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
