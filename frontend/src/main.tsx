5fcf34b754e6c269b85a6505b63c815b2a115cb6
Jorge <jorgehmg2812@academico.ufs.br>
2026-01-31 14:47:37 -0300

    ligação do login ao App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
-//import App from './App.tsx'
-import Login from './pages/Login.tsx'
+import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
+    <App />
   </StrictMode>,
 )
