c2867684067ab35d53892ebe2a216b89de950ecd
Jorge <jorgehmg2812@academico.ufs.br>
2026-01-31 14:56:53 -0300

    ligação de App com tela de Login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,9 @@
 
-import { useState } from 'react';
-import Login from './pages/Login';
-import ListGroup from "./components/ListGroup"; 
+
+import ListGroup from "./components/ListGroup.tsx"; 
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  if (!isAuthenticated) {
-    return <Login />;
-  }
-
+  
   return <div><ListGroup /></div>;
 }
 
