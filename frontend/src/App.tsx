1eb48fad5594f550397f9f83351751947ac1e033
jorge2812 <145615659+jorge2812@users.noreply.github.com>
2026-01-31 15:21:01 -0300

    Jorge (#3)
    * ligação do login ao App
    
    * ligação de App com tela de Login
    
    * ligação da tela de login com o App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 
-
-import ListGroup from "./components/ListGroup.tsx"; 
+import { useState } from 'react';
+import Login from './pages/Login';
+import ListGroup from "./components/ListGroup"; 
 
 function App() {
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  if (!isAuthenticated) {
+    return <Login />;
+  }
+
   return <div><ListGroup /></div>;
 }
 
